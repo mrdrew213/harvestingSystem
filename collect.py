78a85f4db402beba8c057521dc4df717654be158
Andrew Herson <andrew.herson@gmail.com>
2021-08-29 05:15:22 +0100

    modified to leverage functions

--- collect.py
+++ collect.py
@@ -1,6 +1,8 @@
 # External module imports
 import RPi.GPIO as GPIO
 import time
+from read_inputs import read_inputs
+from input_masks import is_watering, is_collecting, is_full, is_charged 
 
 # Pin Definitons:
 ch1 = 12 # master_valve
@@ -27,14 +29,22 @@ GPIO.output(ch4, GPIO.LOW)
 
 try:
 	GPIO.output(ch1, GPIO.HIGH)
-	print("ch1")
+	print("master on")
 	time.sleep(2)
 	GPIO.output(ch2, GPIO.HIGH)
-	print("ch2")
+	print("zone on")
 	while(1):
-		charge = GPIO.input(ch4)
-		if not charge:
+		ins = read_inputs()
+		if not is_charged(ins[0]):
 			print("Warning: citern is half empty")
+                elif is_full(ins[0]):
+                        print("Message: tanks are full")
+	                GPIO.output(ch2, GPIO.LOW)
+	                print("zone 12 off")
+	                time.sleep(2)
+	                GPIO.output(ch1, GPIO.LOW)
+	                print("master off")
+                        break
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
 	GPIO.output(ch2, GPIO.LOW)
 	print("ch2 off")
@@ -42,3 +52,4 @@ except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
 	GPIO.output(ch1, GPIO.HIGH)
 	print("ch1 off")
      	GPIO.cleanup() # cleanup all GPIO
+from read_inputs import read_inputs
